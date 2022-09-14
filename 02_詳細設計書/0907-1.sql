cd15ef100d1f92b5854137c32f0d03f883e43053
Ben Si Tu <80970902+bensitu@users.noreply.github.com>
2022-09-14 10:59:08 +0900

    version 0.2

--- 02_詳細設計書/0907-1.sql
+++ 02_詳細設計書/0907-1.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS `t_employee`;
 CREATE TABLE IF NOT EXISTS `t_employee` (
-  `employee_id` INT(5) NOT NULL AUTO_INCREMENT COMMENT '社員ID',
-  `employee_name` VARCHAR(100) NOT NULL COMMENT '社員名称',
-  `password` VARCHAR(8) NOT NULL COMMENT 'パスワード',
-	`dept_id` VARCHAR(10) COMMENT '所属部門ID',
-  `del_flg` INT(1) DEFAULT '0' COMMENT '削除フラグ',
-	`create_date` DATE COMMENT '作成日時',
-	`create_user_id` INT(5) COMMENT '作成者id',
-	`update_date` DATE COMMENT '更新日時',
-	`update_user_id` INT(5) COMMENT '更新者id',
-  `employee_email` VARCHAR(50) DEFAULT NULL COMMENT '社員メールアドレス',
-  PRIMARY KEY (`employee_id`) USING BTREE
+`employee_id` INT(5) NOT NULL AUTO_INCREMENT=10000 COMMENT '社員ID',
+`employee_name` VARCHAR(100) NOT NULL COMMENT '社員名称',
+`password` VARCHAR(8) NOT NULL COMMENT 'パスワード',
+`dept_id` VARCHAR(10) COMMENT '所属部門ID',
+`del_flg` INT(1) DEFAULT '0' COMMENT '削除フラグ',
+`create_date` DATE COMMENT '作成日時',
+`create_user_id` INT(5) COMMENT '作成者id',
+`update_date` DATE COMMENT '更新日時',
+`update_user_id` INT(5) COMMENT '更新者id',
+`employee_email` VARCHAR(50) DEFAULT NULL COMMENT '社員メールアドレス',
+PRIMARY KEY (`employee_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 SET FOREIGN_KEY_CHECKS = 1;
 
